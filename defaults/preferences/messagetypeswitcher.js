bc74a704b05d95828b3acbf95353da23eff7ecaf
SHIMODA Hiroshi <shimoda@clear-code.com>
2008-08-18 08:22:36 +0000

    説明文を日本語化

---
+++ defaults/preferences/messagetypeswitcher.js
@@ -0,0 +1,2 @@
+pref("extensions.messagetypeswitcher@clear-code.com.name", "chrome://messagetypeswitcher/locale/messagetypeswitcher.properties");
+pref("extensions.messagetypeswitcher@clear-code.com.description", "chrome://messagetypeswitcher/locale/messagetypeswitcher.properties");
