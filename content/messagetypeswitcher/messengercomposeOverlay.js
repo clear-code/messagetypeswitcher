11ccaefc4d705c43a3a103a97942ed34be553dea
SHIMODA Hiroshi <shimoda@clear-code.com>
2008-08-18 06:54:09 +0000

    スタイル付けに関するコマンドを無効化するようにした

--- content/messagetypeswitcher/messengercomposeOverlay.js
+++ content/messagetypeswitcher/messengercomposeOverlay.js
@@ -1,4 +1,5 @@
 var gInitialComposeHtmlMode;
+var isInStartup = true;
 
 window.addEventListener('DOMContentLoaded', function() {
 	window.removeEventListener('DOMContentLoaded', arguments.callee, false);
@@ -22,26 +23,42 @@ window.addEventListener('DOMContentLoaded', function() {
 					var item = document.getElementById('format_plain');
 					item.setAttribute('checked', true);
 					OutputFormatMenuSelect(item);
+					isInStartup = false;
 				}, 0);
 			}
 		$1]]>.toString()
 	));
 
 	eval('window.OutputFormatMenuSelect = '+window.OutputFormatMenuSelect.toSource().replace(
-		'case "format_plain":',
-		<![CDATA[$&
-			try {
-				goDoCommand('cmd_selectAll');
-				goDoCommand('cmd_removeStyles');
-				var doc = gMsgCompose.editor.document;
-				var range = doc.createRange();
-				range.selectNodeContents(doc.documentElement);
-				var sel = doc.defaultView.getSelection();
-				sel.removeAllRanges();
-				// TBD：モード切り替え前の選択範囲の復帰
-			}
-			catch(e) {
-			}
-		]]>.toString()
+		'format_menubar.hidden = gHideMenus;',
+		'toggleHTMLCommands(!gHideMenus); $&'
+	));
+
+	eval('window.doStyleUICommand = '+window.doStyleUICommand.toSource().replace(
+		'{',
+		'$& if (gSendFormat == nsIMsgCompSendFormat.PlainText) return;'
 	));
 }, false);
+
+
+function toggleHTMLCommands(aEnable)
+{
+	if (!aEnable && !isInStartup) clearAllStyles();
+}
+
+function clearAllStyles()
+{
+	try {
+		goDoCommand('cmd_selectAll');
+		goDoCommand('cmd_removeStyles');
+		var doc = gMsgCompose.editor.document;
+		var range = doc.createRange();
+		range.selectNodeContents(doc.documentElement);
+		var sel = doc.defaultView.getSelection();
+		sel.removeAllRanges();
+		// TBD：モード切り替え前の選択範囲の復帰
+	}
+	catch(e) {
+	}
+}
+
